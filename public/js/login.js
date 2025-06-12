eb8837303d3244b08287db3d9ad957a31ff8d426
djadjoel <djadjoel@gmail.com>
2025-06-12 15:50:48 +0700

    update

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,18 @@
+const BASE_API = 'https://aplikasi-todo-laravel-production.up.railway.app';
+console.log("Login to:", BASE_API + '/login');
 function login() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    //console.log({ email, password });
+    console.log("Login to:", BASE_API + '/login');
 
     axios.post(BASE_API + '/login', { email, password })
-
     .then(res => {
         localStorage.setItem('token', res.data.token);
         window.location.href = 'beranda';
     })
     .catch(err => {
-        console.error("Login error:", err.response);
+        console.error("Login error:", err.response?.data || err.message);
         document.getElementById('error').innerText = 'Login gagal';
     });
 }
-
